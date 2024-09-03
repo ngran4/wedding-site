6db93dcd37b04a626130d563c3f0639f2434aa17
Nicole Granville <nicgranville@gmail.com>
2024-09-03 16:21:26 -0700

    add pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import RSVP from './pages/RSVP';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+
 import axios from "axios"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-Wedding site whoop whoop
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route parh="/rsvp" component={RSVP} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/dashboard" component={Dashboard} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Home = () => {
+  return (
+    <div>
+    <h1>Nicole and Ian</h1>
+    <p>Join us in celebrating our special day!</p>
+  </div>
+  )
+}
