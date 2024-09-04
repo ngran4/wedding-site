39588454d484c1d58d18047c0fcc772f2a4e818d
Nicole Granville <nicgranville@gmail.com>
2024-09-04 16:00:03 -0700

    set up initial rsvp form

--- src/pages/RSVP/RSVP.jsx
+++ src/pages/RSVP/RSVP.jsx
@@ -2,10 +2,45 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const RSVP = () => {
+  const [formData, setFormData] = useState({ firstName: '', lastName: ''});
+  const [group, setGroup] = useState(null);
+
+  // handle input changes and update form state
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.get(`/api/guests/group?firstName=${formData.firstName}&lastName=${formData.lastName}`); //***group id instead of first and last name */
+      setGroup(res.data); // update group state with fetched data
+    } catch (error) {
+      alert('error fetching group details');
+    }
+  }
 
   return (
-    <div>RSVP</div>
+    <div>
+      <h1>RSVP Here</h1>
+      {!group ? ( 
+        // if group data not available, show initial form, otherwise show group RSVP form
+      <form>
+        {/* input for putting first and last name same input box? */}
+        <input type="text" name="name" placeholder="Name" required /> 
+        <button type='submit'>Find RSVP</button>
+      </form>
+
+      ) : (
+        <GroupRSVPForm group={group} />
+      )}
+
+    </div>
   )
 }
+
+
+
 
 export default RSVP
