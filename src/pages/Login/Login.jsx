1b0962d03acb042a2bdc663a7861cbf0ea781226
Nicole Granville <nicgranville@gmail.com>
2024-09-11 16:32:33 -0700

    update oauth

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,13 +1,12 @@
 import React, {useState} from 'react';
 import axios from 'axios';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import GoogleLogin from '../components/GoogleLogin'; 
 
 function Login() {
   const [user, setUser] = useState();
 
   return (
-    <GoogleOAuthProvider clientId="163584497548-6uovpebrvioqdepje90dinuagvi60ulc.apps.googleusercontent.com">
+
       <div className="Login">
         <h1>Login</h1>
         <GoogleLogin setUser={setUser} />
@@ -18,6 +17,5 @@ function Login() {
           </div>
         )}
       </div>
-    </GoogleOAuthProvider>
   );
 }
