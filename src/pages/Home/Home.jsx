db984053a11236b411651beda67e44f19f69ad52
Nicole Granville <nicgranville@gmail.com>
2024-12-20 18:57:29 -0800

    where&when block styling

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,19 +12,20 @@ const Home = () => {
         <div className="hero__textbox">
           <p id="hero-intro">Join Us For The Upcoming Wedding Of</p>
           <div id="hero-names">
-            NICOLE <br/>
+            NICOLE <br />
             <div className="inline-container">
-              <div id="ampersand">&   </div>IAN
+              <div id="ampersand">& </div>IAN
             </div>
           </div>
-          <div className="hero-date">
-            9.7.25
-          </div>
+          <div className="hero-date">9.7.25</div>
         </div>
       </div>
 
       <div className="DetailsBlock__container">
-        <div className="DetailsBlock__title">Where&When</div>
+        <div className="DetailsBlock__title">
+          <p id="join-us">JOIN US</p>
+          <div id="where-when">WHERE&WHEN</div>
+        </div>
         <div className="DetailsBlock__details">
           <div className="DetailsBlock__details--date">Sunday, 9.7.25</div>
           <div className="DetailsBlock__details--address">
