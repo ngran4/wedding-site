167f0f3397b5de5770a6ed4d1829f21d8cfb3ffa
Nicole Granville <nicgranville@gmail.com>
2025-01-02 18:20:51 -0800

    home block styling

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css";
 import Footer from "../../components/Footer/Footer";
+import { HiOutlineArrowLongRight } from "react-icons/hi2";
 // import Hero from "./Hero/Hero";
 // import DetailsBlock from "./DetailsBlock/DetailsBlock";
 // import RsvpBlock from "./RsvpBlock/RsvpBlock";
@@ -24,30 +25,69 @@ const Home = () => {
       <div className="DetailsBlock__container">
         <div className="DetailsBlock__title">
           <p id="join-us">JOIN US</p>
-          <div id="where-when">WHERE&WHEN</div>
+          <div className="vl"></div>
+          <div id="where-when">
+            WHERE <br /> & WHEN
+          </div>
         </div>
         <div className="DetailsBlock__details">
-          <div className="DetailsBlock__details--date">SUNDAY, 9.7.25</div>
-          <div className="DetailsBlock__details--address">
+          <div id="DetailsBlock--date">SUNDAY, 9.7.25</div>
+          <div id="DetailsBlock__address">
             Via delle Ville, 2400, 55100 Lucca LU, Italy
           </div>
-          <div className="DetailsBlock__details--time">5pm Ceremony</div>
-          <div className="DetailsBlock__details--time">6pm Reception</div>
-          <div className="DetailsBlock__details--link">
-            <a href="/details">See Full Details</a>
+
+          <div className="DetailsBlock__events">
+          <div id="DetailsBlock__event-time">
+            <div
+              style={{ color: "orange", fontSize: "2rem", display: "inline" }}
+            >
+              4
+            </div>
+            <span style={{ color: "orange", fontSize: "1rem" }}> pm</span>{" "}
+            Ceremony
+          </div>
+          <div id="DetailsBlock__event-time">
+            <div
+              style={{ color: "orange", fontSize: "2rem", display: "inline" }}
+            >
+              5
+            </div>
+            <span style={{ color: "orange", fontSize: "1rem" }}> pm</span>{" "}
+            Aperitivo
+          </div>
+          <div id="DetailsBlock__event-time">
+            <div
+              style={{ color: "orange", fontSize: "2rem", display: "inline" }}
+            >
+              6
+            </div>
+            <span style={{ color: "orange", fontSize: "1rem," }}> pm</span>{" "}
+            Reception
+          </div>
+          </div>
+
+          <div id="DetailsBlock__link">
+            <a href="/details" id="details-link">See Full Details <span id="details-arrow"> <HiOutlineArrowLongRight /></span></a>
           </div>
         </div>
       </div>
 
       <div className="RsvpBlock__container">
-      <div className="RsvpBlock__link">
+        <div className="RsvpBlock__link">
           <div className="RsvpBlock__text">Please confirm your attendance</div>
           <div className="RsvpBlock__link">
             <a href="/rsvp">RSVP</a>
           </div>
         </div>
-        <div className="RsvpBlock__title">PLS RSVP</div>
-
+        <div className="RsvpBlock__title">
+          <p id="commit">READY TO COMMIT?</p>
+          <div className="vl"></div>
+          <div id="pls-rsvp">
+            PLS
+            <br />
+            RSVP
+          </div>
+        </div>
       </div>
 
       <div className="about__container">our story</div>
