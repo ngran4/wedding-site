ffd2acd5d5c5bf0e7af5c0a124ed9c6c6b509857
Nicole Granville <nicgranville@gmail.com>
2025-01-10 12:03:48 -0800

    update server.js

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 require('dotenv').config();
 const express = require("express");
 const session = require('express-session');
-// const passport = require('./config/passport');
 const connectDB = require('./config/database');
 const cors = require('cors');
+const path = require('path');
 
 
 const app = express();
 app.use(cors());
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
 
 // Connect Database
 connectDB();
@@ -18,8 +21,6 @@ app.use(session({
   saveUninitialized: false
 }));
 
-// app.use(passport.initialize());
-// app.use(passport.session());
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -30,8 +31,15 @@ const guestsRouter = require('./routes/api/guests');
 app.use('/api/guests', guestsRouter);
 // app.use('/api/users', userRoutes);
 
-app.get('/', (req, res) => res.send('Hello world!'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+// const passport = require('./config/passport');
+// app.use(passport.initialize());
+// app.use(passport.session());
